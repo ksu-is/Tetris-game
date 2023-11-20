7eefd403f548e4f92135db7850ead3bab8d4b2e0
Ben Zeiher <142267938+bzeiher@users.noreply.github.com>
2023-11-20 14:38:57 -0500

    Add files via upload
    I have added the shaped of the tetris blockes to the code as well as the colors of them as well, this helps set up the game in the shapes and ways you can use them

--- Tetris-Game.py
+++ Tetris-Game.py
@@ -1,7 +1,23 @@
 import pygame 
+import random 
 
-class Game(object):
-def main(self, screen):
-    clock = pygame.time.Clock()
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 300, 600
+GRID_SIZE = 30
+FPS = 10
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
 
     

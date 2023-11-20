26cb9a6c6aab91882d5237738f57e19aa7a4c2c8
Ben Zeiher <142267938+bzeiher@users.noreply.github.com>
2023-11-20 14:40:33 -0500

    Add files via upload
    this adds the game window and the grid of the game so that the layout of the blocks can stay within a grid and not just go anywhere outside of the grid

--- Tetris-Game.py
+++ Tetris-Game.py
@@ -20,4 +20,37 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 ORANGE = (255, 165, 0)
 
+# Shapes and their colors
+SHAPES = [
+    [[1, 1, 1, 1]],  # I
+    [[1, 1, 1], [1]],  # L
+    [[1, 1, 1], [0, 0, 1]],  # J
+    [[1, 1], [1, 1]],  # O
+    [[1, 1, 1], [0, 1]],  # S
+    [[1, 1, 1], [1, 0]],  # T
+    [[1, 1], [0, 1, 1]],  # Z
+]
+
+SHAPES_COLORS = [CYAN, ORANGE, BLUE, YELLOW, GREEN, MAGENTA, RED]
+
+# Initialize game window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
+
+# Function to draw the grid
+def draw_grid():
+    for x in range(0, WIDTH, GRID_SIZE):
+        pygame.draw.line(screen, WHITE, (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT, GRID_SIZE):
+        pygame.draw.line(screen, WHITE, (0, y), (WIDTH, y))
+
+# Function to draw a shape on the grid
+def draw_shape(shape, pos, color):
+    for y, row in enumerate(shape):
+        for x, value in enumerate(row):
+            if value:
+                pygame.draw.rect(screen, color, (pos[0] + x * GRID_SIZE, pos[1] + y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+                pygame.draw.rect(screen, WHITE, (pos[0] + x * GRID_SIZE, pos[1] + y * GRID_SIZE, GRID_SIZE, GRID_SIZE), 1)
+
     
